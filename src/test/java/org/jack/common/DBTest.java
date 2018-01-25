c787dde79c93e65fcff09e87e7767f3d85e0c8ef
zhangw <zhangw@yuminsoft.com>
2018-01-25 14:56:58 +0800

    table columns

--- src/test/java/org/jack/common/DBTest.java
+++ src/test/java/org/jack/common/DBTest.java
@@ -1,6 +1,13 @@
 package org.jack.common;
 
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.jack.common.util.DBUtils;
 import org.junit.Test;
@@ -20,21 +27,63 @@ public class DBTest extends BaseTest{
 		DEV_CREDIT_ZX.setPassword("123456");
 	}
 	@Test
-	public void testMySql() {
+	public void testColumns(){
+		DBUtils.ConnectionInfo connectionInfo=DEV_CREDIT_ZX;
+		String tableName="bms_loan_base";
+		
+		tableName="T_PBCCRC_REPORT";
 		try {
-			DBUtils.getConnection(DEV_BMS);
-			log("Connection success");
+			List<String> columns=getTableColumns(tableName, connectionInfo);
+			log("ColumnCount:"+columns.size());
+			log(columns);
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
+	}
+	private List<String> getTableColumns(String tableName,DBUtils.ConnectionInfo connectionInfo) throws SQLException{
+		Connection connection=DBUtils.getConnection(connectionInfo);
+		DatabaseMetaData metaData=connection.getMetaData();
+		String dbType=metaData.getDatabaseProductName();
+		StringBuilder sql=new StringBuilder();
+		sql.append("SELECT * FROM  ");
+		sql.append(tableName);
+		if(dbType.toLowerCase().contains("mysql")){
+			sql.append(" limit 0");
+		}else if(dbType.toLowerCase().contains("oracle")){
+			sql.append(" where rownum<1");
+		}else if(dbType.toLowerCase().contains("db2")){
+			sql.append(" fetch   first  1 rows  only");
+		}
+		List<String> columns=new ArrayList<String>();
+		Statement statement=connection.createStatement();
+		ResultSet rs=statement.executeQuery(sql.toString());
+		ResultSetMetaData rsetaData=rs.getMetaData();
+		int count=rsetaData.getColumnCount();
+		
+		for(int i=1;i<=count;i++){
+			columns.add(rsetaData.getColumnName(i));
+		}
+		return columns;
+		
+	}
+	@Test
+	public void testMySQL() {
+		testConnection(DEV_BMS);
 	}
 	@Test
 	public void testOracel() {
+		testConnection(DEV_CREDIT_ZX);
+	}
+	private void testConnection(DBUtils.ConnectionInfo connectionInfo) {
 		try {
-			DBUtils.getConnection(DEV_CREDIT_ZX);
+			Connection connection=DBUtils.getConnection(connectionInfo);
+			DatabaseMetaData metaData=connection.getMetaData();
+			log(metaData.getDatabaseProductName());
+			log(metaData.getDatabaseProductVersion());
 			log("Connection success");
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 	}
+	
 }
