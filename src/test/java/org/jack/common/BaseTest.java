f63f301281b07f268df19db055a7578a151ee861
zhangw <zhangw@yuminsoft.com>
2018-03-09 13:33:28 +0800

    BaseTest

--- src/test/java/org/jack/common/BaseTest.java
+++ src/test/java/org/jack/common/BaseTest.java
@@ -1,7 +1,47 @@
 package org.jack.common;
 
 public abstract class BaseTest {
-	public void log(Object msg){
+	protected void log(double...msgs) {
+		for(double msg:msgs){
+			log(msg);
+		}
+	}
+	protected void log(float...msgs) {
+		for(float msg:msgs){
+			log(msg);
+		}
+	}
+	protected void log(long...msgs) {
+		for(long msg:msgs){
+			log(msg);
+		}
+	}
+	protected void log(int...msgs) {
+		for(int msg:msgs){
+			log(msg);
+		}
+	}
+	protected void log(char...msgs) {
+		for(char msg:msgs){
+			log(msg);
+		}
+	}
+	protected void log(short...msgs) {
+		for(short msg:msgs){
+			log(msg);
+		}
+	}
+	protected void log(byte...msgs) {
+		for(byte msg:msgs){
+			log(msg);
+		}
+	}
+	protected void log(Object...msgs) {
+		for(Object msg:msgs){
+			log(msg);
+		}
+	}
+	protected void log(Object msg) {
 		System.out.println(msg);
 	}
 }
