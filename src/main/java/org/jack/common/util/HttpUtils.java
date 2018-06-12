4b60db44c3c8985871e726fabf85e318a8dc70f2
zhangw <zhangw@yuminsoft.com>
2018-06-12 16:13:41 +0800

    httpUtils

--- src/main/java/org/jack/common/util/HttpUtils.java
+++ src/main/java/org/jack/common/util/HttpUtils.java
@@ -1,10 +1,13 @@
 package org.jack.common.util;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.http.Consts;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
@@ -14,11 +17,12 @@ import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.entity.ContentType;
 import org.apache.http.entity.StringEntity;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
-
 /**
  * http 工具类
  * @author YM10177
@@ -90,6 +94,49 @@ public class HttpUtils {
 		entity.setContentType("application/json");
 		httpPost.setEntity(entity);
 		return doRequest(httpPost);
+	}
+	public static class MultipartInfo{
+		private String fileKey;
+		private String fileName;
+		private InputStream inputStream;
+		public String getFileKey() {
+			return fileKey;
+		}
+		public void setFileKey(String fileKey) {
+			this.fileKey = fileKey;
+		}
+		public String getFileName() {
+			return fileName;
+		}
+		public void setFileName(String fileName) {
+			this.fileName = fileName;
+		}
+		public InputStream getInputStream() {
+			return inputStream;
+		}
+		public void setInputStream(InputStream inputStream) {
+			this.inputStream = inputStream;
+		}
+	}
+	public String upload(String url,List<MultipartInfo> multipartInfos,Map<String,Object> paramMap) throws ClientProtocolException, IOException{
+		HttpPost httpPost = new HttpPost(url);
+        MultipartEntityBuilder builder = MultipartEntityBuilder.create();
+        for(MultipartInfo multipartInfo:multipartInfos){
+        	builder.addBinaryBody(multipartInfo.getFileKey(), multipartInfo.getInputStream(), ContentType.MULTIPART_FORM_DATA, multipartInfo.getFileName());
+        }
+        ContentType contentType=ContentType.create("text/plain", Consts.UTF_8);
+        for(Map.Entry<String,Object> entry:paramMap.entrySet()){
+        	if(entry.getValue()==null){
+        		continue;
+        	}
+        	builder.addTextBody(entry.getKey(), entry.getValue().toString(),contentType);
+        }
+        HttpEntity entity = builder.build();
+        httpPost.setEntity(entity);
+        return doRequest(httpPost);
+	}
+	public String upload(String url,MultipartInfo multipartInfo,Map<String,Object> paramMap) throws ClientProtocolException, IOException{
+		return upload(url, Collections.singletonList(multipartInfo), paramMap);
 	}
 	/**
 	 * get 请求
