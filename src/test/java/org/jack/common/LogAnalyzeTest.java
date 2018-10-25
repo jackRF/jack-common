613efa2bf1006c28362af3be3694c5d84e306b02
zhangw <zhangw@yuminsoft.com>
2018-10-25 17:37:50 +0800

    日志分析

--- src/test/java/org/jack/common/LogAnalyzeTest.java
+++ src/test/java/org/jack/common/LogAnalyzeTest.java
@@ -17,6 +17,7 @@ import org.springframework.util.StringUtils;
 
 public class LogAnalyzeTest extends BaseTest {
 	private int i=0;
+	private LocalInfo lastlocalInfo;
 	@Test
 	public void testAnalyze() throws IOException {
 		
@@ -31,18 +32,18 @@ public class LogAnalyzeTest extends BaseTest {
 				i++;
 				LineInfo lineInfo=null;
 				try{
-					lineInfo=parseLine(line);
+					lineInfo=parseLine(line,i,lastlocalInfo);
 					
 					if(lineInfo==null){
 						return;
 					}
-					lineInfo.setLineIndex(i);
 				}catch(Exception e){
 					return;
 				}
 				LocalInfo localInfo=threadMap.get(lineInfo.getThreadId());
 				if(localInfo==null){
 					localInfo=new LocalInfo();
+					localInfo.setThreadId(lineInfo.getThreadId());
 					threadMap.put(lineInfo.getThreadId(), localInfo);
 				}else{
 					applyRule(localInfo,lineInfo);
@@ -52,6 +53,8 @@ public class LogAnalyzeTest extends BaseTest {
 				if(StringUtils.hasText(lineInfo.getBizId())){
 					localInfo.setBizId(lineInfo.getBizId());
 				}
+				localInfo.setMethodEnd(lineInfo.isMethodEnd());
+				lastlocalInfo=localInfo;
 			}
 
 			
@@ -66,34 +69,44 @@ public class LogAnalyzeTest extends BaseTest {
 		}
 		long duration=time2.getTime()-time1.getTime();
 		if(duration>2000){
+			if(localInfo.isMethodEnd()){
+				return;
+			}
 			log(String.format("线程%s在%d-%d行耗时%ds",lineInfo.getThreadId(), localInfo.getLastLoglineIndex(),lineInfo.getLineIndex(),duration/1000));
 		}
 	}
 	@Test
 	public void testParseLine() {
-		parseLine("2018-10-24 16:00:00[INFO][com.ymkj.base.core.biz.filter.BizLoggerFilter.invoke()][DubboServerHandler-10.100.40.113:20880-thread-487]:");
+		parseLine("2018-10-24 16:00:00[INFO][com.ymkj.base.core.biz.filter.BizLoggerFilter.invoke()][DubboServerHandler-10.100.40.113:20880-thread-487]:",1,null);
 
 	}
 	private Pattern threadPattern=Pattern.compile(":(\\d+-thread-\\d+)]:$");
-	private LineInfo parseLine(String line){
+	private LineInfo parseLine(String line, int lineIndex, LocalInfo lastlocalInfo){
 		if(!StringUtils.hasText(line)){
 			return null;
 		}
 		Matcher matcher=threadPattern.matcher(line);
-		if(!matcher.find()){
+		LineInfo lineInfo=null;
+		if(matcher.find()){
+			String threadId=matcher.group(1);
+			lineInfo=new LineInfo();
+			lineInfo.setThreadId(threadId);
+			Date time;
+			try {
+				time = DateUtils.parseDate(line.substring(0, 19), "yyyy-MM-dd HH:mm:ss");
+				lineInfo.setTime(time);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}else if(lastlocalInfo!=null){
+			lineInfo=new LineInfo();
+			lineInfo.setThreadId(lastlocalInfo.getThreadId());
+			lineInfo.setTime(lastlocalInfo.getLastLogTime());
+		}else{
 			return null;
 		}
-		String threadId=matcher.group(1);
-		LineInfo lineInfo=new LineInfo();
-		lineInfo.setThreadId(threadId);
-		Date time;
-		try {
-			time = DateUtils.parseDate(line.substring(0, 19), "yyyy-MM-dd HH:mm:ss");
-			lineInfo.setTime(time);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		
+		lineInfo.setLineIndex(lineIndex);
+		lineInfo.setMethodEnd(line.startsWith("==========结束调用服务接口"));
 		return lineInfo;
 	}
 	static class LineInfo{
@@ -102,6 +115,14 @@ public class LogAnalyzeTest extends BaseTest {
 		private String bizId;
 		private int lineIndex;
 		private Map<String,Object> props;
+		private boolean isMethodEnd;
+		
+		public boolean isMethodEnd() {
+			return isMethodEnd;
+		}
+		public void setMethodEnd(boolean isMethodEnd) {
+			this.isMethodEnd = isMethodEnd;
+		}
 		public int getLineIndex() {
 			return lineIndex;
 		}
@@ -134,9 +155,17 @@ public class LogAnalyzeTest extends BaseTest {
 		}
 	}
 	static class LocalInfo{
+		private String threadId;
 		private Date lastLogTime;
 		private int lastLoglineIndex;
 		private String bizId;
+		private boolean isMethodEnd;
+		public String getThreadId() {
+			return threadId;
+		}
+		public void setThreadId(String threadId) {
+			this.threadId = threadId;
+		}
 		public Date getLastLogTime() {
 			return lastLogTime;
 		}
@@ -154,6 +183,12 @@ public class LogAnalyzeTest extends BaseTest {
 		}
 		public void setBizId(String bizId) {
 			this.bizId = bizId;
+		}
+		public boolean isMethodEnd() {
+			return isMethodEnd;
+		}
+		public void setMethodEnd(boolean isMethodEnd) {
+			this.isMethodEnd = isMethodEnd;
 		}
 		
 	}
